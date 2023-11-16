17ae9d8ea339fa3a804f61f3b2989ba20939e9c0
Gabriel Frattini <gabbefrattini@gmail.com>
2023-11-16 17:50:50 +0100

    add found bug in spec.txt

--- src/test/java/com/lab/NextIncompleteTests.java
+++ src/test/java/com/lab/NextIncompleteTests.java
@@ -8,21 +8,18 @@ public class NextIncompleteTests {
      @Test
      public void test_m_part1() {
 
-          // Preparation, creates a WorkSchedule (with hour 0 and 1) and adds one employee
           WorkSchedule workSchedule = new WorkSchedule(2);
           int currenttime = 0;
           workSchedule.setRequiredNumber(1, currenttime, 1);
 
           int starttime = workSchedule.nextIncomplete(currenttime);
 
-          // Next incomplete hour from 0 is 1
           assertEquals(currenttime + 1, starttime);
      }
 
      @Test
      public void test_m_part2() {
 
-          // Preparation, creates a WorkSchedule (with hour 0 and 1) and adds one employee
           WorkSchedule workSchedule = new WorkSchedule(7); // Index out of bounds exception if it is <= 7
           int currenttime = 5;
           workSchedule.setRequiredNumber(1, currenttime, 6);
