19833ac4885679f5665c40a7da22f42a1e47caa0
Falckishen <samuelfa@student.chalmers.se>
2023-11-29 17:13:04 +0100

    Added some tests for lab1 blackbox testing

--- src/test/java/com/lab/lab1/NextIncompleteTests.java
+++ src/test/java/com/lab/lab1/NextIncompleteTests.java
@@ -12,11 +12,11 @@ public class NextIncompleteTests {
 
           WorkSchedule workSchedule = new WorkSchedule(10);
 
-          int currentTime = 0;
+          int currentTime = 1;
           workSchedule.setRequiredNumber(1, 3, 5);
           int nextIncomplete = workSchedule.nextIncomplete(currentTime);
 
-          // Next incomplete hour from 0 is 1
+          // Next incomplete hour from 1 is 3
           assertEquals(3, nextIncomplete);
      }
 
@@ -27,9 +27,69 @@ public class NextIncompleteTests {
 
           int currentTime = 1;
           workSchedule.setRequiredNumber(1, 3, 5);
+          workSchedule.addWorkingPeriod("TestEmployee", 3, 5);
           int nextIncomplete = workSchedule.nextIncomplete(currentTime);
 
-          // Next incomplete hour from 1 is 3
-          assertEquals(3, nextIncomplete);
+          // No incomplete hours
+          assertEquals(-1, nextIncomplete);
+     }
+
+     @Test
+     public void test_m_part3() {
+
+          WorkSchedule workSchedule = new WorkSchedule(10);
+
+          int currentTime = 1;
+          workSchedule.setRequiredNumber(1, 3, 5);
+          workSchedule.addWorkingPeriod("TestEmployee1", 3, 3);
+          workSchedule.addWorkingPeriod("TestEmployee2", 4, 5);
+          int nextIncomplete = workSchedule.nextIncomplete(currentTime);
+
+          // No incomplete hours
+          assertEquals(-1, nextIncomplete);
+     }
+
+     @Test
+     public void test_m_part4() {
+
+          WorkSchedule workSchedule = new WorkSchedule(10);
+
+          int currentTime = 1;
+          workSchedule.setRequiredNumber(1, 3, 5);
+          workSchedule.addWorkingPeriod("TestEmployee1", 3, 4);
+          workSchedule.addWorkingPeriod("TestEmployee2", 4, 5);
+          int nextIncomplete = workSchedule.nextIncomplete(currentTime);
+
+          // Hour 4 has too many employees, but no incomplete hours
+          assertEquals(-1, nextIncomplete);
+     }
+
+     @Test
+     public void test_m_part5() {
+
+          WorkSchedule workSchedule = new WorkSchedule(10);
+
+          int currentTime = 1;
+          workSchedule.setRequiredNumber(1, 3, 5);
+          workSchedule.setRequiredNumber(1, 6, 7);
+          workSchedule.addWorkingPeriod("TestEmployee1", 3, 5);
+          int nextIncomplete = workSchedule.nextIncomplete(currentTime);
+
+          // Next incomplete hour from 1 is 6
+          assertEquals(6, nextIncomplete);
+     }
+
+     @Test
+     public void test_m_part6() {
+
+          WorkSchedule workSchedule = new WorkSchedule(10);
+
+          int currentTime = 1;
+          workSchedule.setRequiredNumber(1, 3, 7);
+          workSchedule.addWorkingPeriod("TestEmployee1", 3, 5);
+          int nextIncomplete = workSchedule.nextIncomplete(currentTime);
+
+          // Next incomplete hour from 1 is 6
+          assertEquals(6, nextIncomplete);
      }
 }
